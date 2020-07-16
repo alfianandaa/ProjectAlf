ed3c417b0ed30a70032d5fc4af36d4258643e2da
Angga <angga@linuxmail.org>
2020-07-16 22:45:25 +0700

    getmusic: replace sdd with vsong

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -3,6 +3,9 @@
 # All rights reserved.
 
 import asyncio
+import glob
+from bs4 import BeautifulSoup
+import requests
 import deezloader
 import os
 import shutil
@@ -14,8 +17,33 @@ from userbot import bot, CMD_HELP, DEEZER_ARL_TOKEN, TEMP_DOWNLOAD_DIRECTORY
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeAudio
+from telethon.tl.types import DocumentAttributeVideo
+
 
 from userbot.events import register
+from userbot.utils import progress
+
+# For getvideosong
+
+
+def getmusicvideo(cat):
+    search = cat
+    headers = {
+        "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    }
+    html = requests.get(
+        "https://www.youtube.com/results?search_query=" +
+        search,
+        headers=headers).text
+    soup = BeautifulSoup(html, "html.parser")
+    for link in soup.find_all("a"):
+        if "/watch?v=" in link.get("href"):
+            # May change when Youtube Website may get updated in the future.
+            video_link = link.get("href")
+            break
+    video_link = "http://www.youtube.com/" + video_link
+    command = 'youtube-dl -f "[filesize<50M]" --merge-output-format mp4 ' + video_link
+    os.system(command)
 
 
 @register(outgoing=True, pattern="^.netease(?: |$)(.*)")
@@ -45,31 +73,70 @@ async def _(event):
     await event.delete()
 
 
-@register(outgoing=True, pattern="^.sdd(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.vsong(?: |$)(.*)")
 async def _(event):
-    if event.fwd_from:
-        return
-    d_link = event.pattern_match.group(1)
-    if ".com" not in d_link:
-        await event.edit("` I need a link to download something pro.`**(._.)**")
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("`Wait..! I am finding your videosong..`")
+    elif reply.message:
+        query = reply.message
+        await event.edit("`Wait..! I am finding your videosong..`")
     else:
-        await event.edit("**Initiating Download!**")
-    chat = "@MusicHuntersBot"
-    async with bot.conversation(chat) as conv:
-        try:
-            msg_start = await conv.send_message("/start")
-            response = await conv.get_response()
-            msg = await conv.send_message(d_link)
-            details = await conv.get_response()
-            song = await conv.get_response()
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.edit("**Error:** `unblock` @MusicHuntersBot `and retry!`")
-            return
-        await bot.send_file(event.chat_id, song, caption=details.text)
-        await event.client.delete_messages(conv.chat_id,
-                                           [msg_start.id, response.id, msg.id, details.id, song.id])
-        await event.delete()
+        await event.edit("`What I am Supposed to find?`")
+        return
+    getmusicvideo(query)
+    l = glob.glob(("*.mp4")) + glob.glob(("*.mkv")) + glob.glob(("*.webm"))
+    if l:
+        await event.edit("`Yeah..! i found something..`")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]
+    metadata = extractMetadata(createParser(loa))
+    duration = 0
+    width = 0
+    height = 0
+    if metadata.has("duration"):
+        duration = metadata.get("duration").seconds
+    if metadata.has("width"):
+        width = metadata.get("width")
+    if metadata.has("height"):
+        height = metadata.get("height")
+    await event.edit("`Uploading video.. Please wait..`")
+    os.system("cp *mp4 thumb.mp4")
+    os.system("ffmpeg -i thumb.mp4 -vframes 1 -an -s 480x360 -ss 5 thumb.jpg")
+    thumb_image = "thumb.jpg"
+    c_time = time.time()
+    await event.client.send_file(
+        event.chat_id,
+        loa,
+        force_document=False,
+        thumb=thumb_image,
+        allow_cache=False,
+        caption=query,
+        supports_streaming=True,
+        reply_to=reply_to_id,
+        attributes=[
+            DocumentAttributeVideo(
+                duration=duration,
+                w=width,
+                h=height,
+                round_message=False,
+                supports_streaming=True,
+            )
+        ],
+        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            progress(d, t, event, c_time, "[UPLOAD]", loa)
+        ),
+    )
+    await event.delete()
+    os.remove(thumb_image)
+    os.system("rm -rf *.mkv")
+    os.system("rm -rf *.mp4")
+    os.system("rm -rf *.webm")
 
 
 @register(outgoing=True, pattern="^.smd(?: |$)(.*)")
@@ -244,8 +311,8 @@ CMD_HELP.update({
     "getmusic":
     ">`.netease <Artist - Song Title>`"
     "\nUsage: Download music with @WooMaiBot"
-    "\n\n>`.sdd <Spotify/Deezer Link>`"
-    "\nUsage: Download music from Spotify or Deezer"
+    "\n\n>`.vsong` **Artist - Song Title**"
+    "\nUsage: Finding and uploading videoclip."
     "\n\n>`.smd <Artist - Song Title>`"
     "\nUsage: Download music from Spotify"
     "\n\n>`.deez <spotify/deezer link> FORMAT`"
