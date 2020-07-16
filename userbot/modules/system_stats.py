9fa59123e95ef077ab171c906665ae28d93f0560
Angga <angga@linuxmail.org>
2020-07-16 22:45:25 +0700

    system_stats: add spc module

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -1,16 +1,20 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot module for getting information about the server. """
+import platform
+import sys
 import time
+import psutil
+from datetime import datetime
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from platform import python_version, uname
 from shutil import which
 from os import remove
-from telethon import version
+from telethon import __version__, version
 
 from userbot import bot, CMD_HELP, ALIVE_NAME, ALIVE_LOGO, ALIVE_USERNAME, UPSTREAM_REPO_BRANCH, StartTime
 from userbot.events import register
@@ -48,7 +52,65 @@ async def get_readable_time(seconds: int) -> str:
 
     return up_time
 
-@register(outgoing=True, pattern="^.sysd$")
+@register(outgoing=True, pattern=r"^\.spc")
+async def psu(event):
+    uname = platform.uname()
+    softw = f"**System Information**\n"
+    softw += f"`System   : {uname.system}`\n"
+    softw += f"`Release  : {uname.release}`\n"
+    softw += f"`Version  : {uname.version}`\n"
+    softw += f"`Machine  : {uname.machine}`\n"
+    # Boot Time
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
+    softw += f"`Boot Time: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}`\n"
+    # CPU Cores
+    cpuu = f"**CPU Info**\n"
+    cpuu += "`Physical cores   : " + \
+        str(psutil.cpu_count(logical=False)) + "`\n"
+    cpuu += "`Total cores      : " + \
+        str(psutil.cpu_count(logical=True)) + "`\n"
+    # CPU frequencies
+    cpufreq = psutil.cpu_freq()
+    cpuu += f"`Max Frequency    : {cpufreq.max:.2f}Mhz`\n"
+    cpuu += f"`Min Frequency    : {cpufreq.min:.2f}Mhz`\n"
+    cpuu += f"`Current Frequency: {cpufreq.current:.2f}Mhz`\n\n"
+    # CPU usage
+    cpuu += f"**CPU Usage Per Core**\n"
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
+        cpuu += f"`Core {i}  : {percentage}%`\n"
+    cpuu += f"**Total CPU Usage**\n"
+    cpuu += f"`All Core: {psutil.cpu_percent()}%`\n"
+    # RAM Usage
+    svmem = psutil.virtual_memory()
+    memm = f"**Memory Usage**\n"
+    memm += f"`Total     : {get_size(svmem.total)}`\n"
+    memm += f"`Available : {get_size(svmem.available)}`\n"
+    memm += f"`Used      : {get_size(svmem.used)}`\n"
+    memm += f"`Percentage: {svmem.percent}%`\n"
+    # Bandwidth Usage
+    bw = "**Bandwith Usage**\n"
+    bw += f"`Upload  : {get_size(psutil.net_io_counters().bytes_sent)}`\n"
+    bw += f"`Download: {get_size(psutil.net_io_counters().bytes_recv)}`\n"
+    help_string = f"{str(softw)}\n"
+    help_string += f"{str(cpuu)}\n"
+    help_string += f"{str(memm)}\n"
+    help_string += f"{str(bw)}\n"
+    help_string += f"**Engine Info**\n"
+    help_string += f"`Python {sys.version}`\n"
+    help_string += f"`Telethon {__version__}`"
+    await event.edit(help_string)
+
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+
+@register(outgoing=True, pattern=r"^\.sysd$")
 async def sysdetails(sysd):
     """ For .sysd command, get system info using neofetch. """
     if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
@@ -196,21 +258,18 @@ async def amireallyalivereset(ureset):
     await ureset.edit("`" "Successfully reset user for alive!" "`")
 
 
-CMD_HELP.update({
-    "sysd":
-    ">`.sysd`"
-    "\nUsage: Shows system information using neofetch.",
-    "botver":
-    ">`.botver`"
-    "\nUsage: Shows the userbot version.",
-    "pip":
-    ">`.pip <module(s)>`"
-    "\nUsage: Does a search of pip modules(s).",
-    "alive":
-    ">`.alive | .on`"
-    "\nUsage: Type .alive or .on to see wether your bot is working or not."
-    "\n\n>`.aliveu <text>`"
-    "\nUsage: Changes the 'user' in alive to the text you want."
-    "\n\n>`.resetalive`"
-    "\nUsage: Resets the user to default."
-})
+CMD_HELP.update({"sysd": ">`.sysd`"
+                 "\nUsage: Shows system information using neofetch.\n\n"
+                 ">`.spc`"
+                 "\nUsage: Show system specification.",
+                 "botver": ">`.botver`"
+                 "\nUsage: Shows the userbot version.",
+                 "pip": ">`.pip <module(s)>`"
+                 "\nUsage: Does a search of pip modules(s).",
+                 "alive": ">`.alive`"
+                 "\nUsage: Type .alive to see wether your bot is working or not."
+                 "\n\n>`.aliveu <text>`"
+                 "\nUsage: Changes the 'user' in alive to the text you want."
+                 "\n\n>`.resetalive`"
+                 "\nUsage: Resets the user to default.",
+                 })
