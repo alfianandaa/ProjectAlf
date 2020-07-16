95a4213fd217ad73840d305fe39b66053806c5fc
Angga <angga@linuxmail.org>
2020-07-16 22:45:25 +0700

    speedtest: add some info

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -23,18 +23,21 @@ async def speedtst(spd):
     test.results.share()
     result = test.results.dict()
 
-    await spd.edit("`"
-                   "Started at "
-                   f"{result['timestamp']} \n\n"
-                   "Download "
-                   f"{speed_convert(result['download'])} \n"
-                   "Upload "
-                   f"{speed_convert(result['upload'])} \n"
-                   "Ping "
-                   f"{result['ping']} \n"
-                   "ISP "
-                   f"{result['client']['isp']}"
-                   "`")
+    output = f"Started at `{result['timestamp']}`\n\n"
+    output += "`Client:`\n"
+    output += f"ISP: `{result['client']['isp']}`\n"
+    output += f"Country: `{result['client']['country']}`\n\n"
+    output += "`Server:`\n"
+    output += f"Name: `{result['server']['name']}`\n"
+    output += f"Country: `{result['server']['country']}, {result['server']['cc']}`\n"
+    output += f"Sponsor: `{result['server']['sponsor']}`\n"
+    output += f"Latency: `{result['server']['latency']}`\n\n"
+    output += "`Speed:`\n"
+    output += f"Ping: `{result['ping']}`\n"
+    output += f"Download: `{speed_convert(result['download'])}`\n"
+    output += f"Upload: `{speed_convert(result['upload'])}` "
+    await spd.delete()
+    await spd.client.send_message(spd.chat_id, output)
 
 
 def speed_convert(size):
