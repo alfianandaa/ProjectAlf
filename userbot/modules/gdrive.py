0ac344168693228a53f91fdfb62c4e060ee8755f
Alfiananda P.A <68804090+alfianandaa@users.noreply.github.com>
2020-07-27 18:56:53 +0700

    Gdrive: fix
    Thanks AnggaR69s

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -587,7 +587,8 @@ async def upload(gdrive, service, file_path, file_name, mimeType):
         "mimeType": mimeType,
     }
     try:
-        pass
+        if parent_Id is not None:
+            pass
     except NameError:
         if G_DRIVE_FOLDER_ID is not None:
             body['parents'] = [G_DRIVE_FOLDER_ID]
@@ -673,7 +674,8 @@ async def task_directory(gdrive, service, folder_path):
 async def reset_parentId():
     global parent_Id
     try:
-        pass
+        if parent_Id is not None:
+            pass
     except NameError:
         if G_DRIVE_FOLDER_ID is not None:
             parent_Id = G_DRIVE_FOLDER_ID
@@ -807,7 +809,8 @@ async def google_drive_managers(gdrive):
             'mimeType': 'application/vnd.google-apps.folder',
         }
         try:
-            pass
+            if parent_Id is not None:
+                pass
         except NameError:
             if G_DRIVE_FOLDER_ID is not None:
                 metadata['parents'] = [G_DRIVE_FOLDER_ID]
