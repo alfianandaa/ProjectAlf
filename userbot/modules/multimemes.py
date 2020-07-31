bc0e11971feed245803f7ce361bbc8df325febd1
aidilaryanto <aidilaryanto18@gmail.com>
2020-07-31 23:24:48 +0800

    sangmata: merge to multimemes & change method to search by id

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -15,6 +15,7 @@ from random import randint, uniform
 from telethon import events
 from PIL import Image, ImageEnhance, ImageOps
 import os
+from userbot.events import register
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import (
     DocumentAttributeFilename,
@@ -432,10 +433,56 @@ def deEmojify(inputString: str) -> str:
     return re.sub(EMOJI_PATTERN, '', inputString)
 
 
+@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
+async def lastname(steal):
+    if steal.fwd_from:
+        return
+    if not steal.reply_to_msg_id:
+        await steal.edit("```Reply to any user message.```")
+        return
+    message = await steal.get_reply_message()
+    chat = "@SangMataInfo_bot"
+    user_id = message.sender.id
+    id = f"/search_id {user_id}"
+    if message.sender.bot:
+        await steal.edit("```Reply to actual users message.```")
+        return
+    await steal.edit("```Sit tight while I steal some data from NASA```")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await steal.reply("```Please unblock @sangmatainfo_bot and try again```")
+                return
+            if response.text.startswith("No records"):
+                await steal.edit("```No records found for this user```")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id]
+                )
+                return
+            else:
+                respond = await conv.get_response()
+                await steal.edit(f"{response.message}")
+            await steal.client.delete_messages(
+                conv.chat_id, [msg.id, r.id, response.id, respond.id]
+            )
+    except TimeoutError:
+        return await steal.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
+
+
 CMD_HELP.update({
     "memify":
         ">`.mmf texttop ; textbottom`"
         "\nUsage: Reply a sticker/image/gif and send with cmd."
+})
+
+CMD_HELP.update({
+    "sangmata":
+        ".sg \
+          \nUsage: Steal ur or friend name."
 })
 
 CMD_HELP.update({
