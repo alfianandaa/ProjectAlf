0bed001cccb770bc2735bbf83b11452b84d740cd
Alfiananda P.A <genengbendo12@gmail.com>
2020-08-24 18:10:21 +0000

    pylint: auto fixes
    Signed-off-by: Alfiananda P.A <genengbendo12@gmail.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -10,7 +10,16 @@ from random import choice, randint
 
 from telethon.events import StopPropagation
 
-from userbot import PM_AUTO_BAN, AFKREASON, BOTLOG, BOTLOG_CHATID, CMD_HELP, COUNT_MSG, ISAFK, USERS  # pylint: disable=unused-imports
+from userbot import (
+    AFKREASON,
+    BOTLOG,  # pylint: disable=unused-imports
+    BOTLOG_CHATID,
+    CMD_HELP,
+    COUNT_MSG,
+    ISAFK,
+    PM_AUTO_BAN,
+    USERS,
+)
 from userbot.events import register
 
 # ========================= CONSTANTS ============================
