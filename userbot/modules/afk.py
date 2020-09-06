f1401083c8aee0a312122cefc782a758ef96299a
Alfiananda P.A <genengbendo12@gmail.com>
2020-09-06 12:01:30 +0700

    afk:fix undefined import improve from 0bed001
    Signed-off-by: Alfiananda P.A <genengbendo12@gmail.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -10,9 +10,18 @@ from random import choice, randint
 
 from telethon.events import StopPropagation
 
-from userbot import BOTLOG  # pylint: disable=unused-imports
-from userbot import AFKREASON, BOTLOG_CHATID, CMD_HELP, PM_AUTO_BAN
 from userbot.events import register
+
+from userbot import (  # noqa pylint: disable=unused-import isort:skip
+    AFKREASON,
+    BOTLOG,
+    BOTLOG_CHATID,
+    CMD_HELP,
+    COUNT_MSG,
+    ISAFK,
+    PM_AUTO_BAN,
+    USERS,
+)
 
 # ========================= CONSTANTS ============================
 AFKSTR = [
