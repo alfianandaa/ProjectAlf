9d3ca4afc84ce1ebb3cb44c1159f3ec2dd0edfbb
Alfiananda P.A <genengbendo12@gmail.com>
2020-09-23 00:34:16 +0700

    getmusic: vsong: add exception handle * just in case
    Signed-off-by: Alfiananda P.A <genengbendo12@gmail.com>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -114,48 +114,55 @@ async def _(event):
     else:
         await event.edit(f"`Sorry..! i can't find anything with` **{query}**")
         return
-    loa = l[0]
-    metadata = extractMetadata(createParser(loa))
-    duration = 0
-    width = 0
-    height = 0
-    if metadata.has("duration"):
-        duration = metadata.get("duration").seconds
-    if metadata.has("width"):
-        width = metadata.get("width")
-    if metadata.has("height"):
-        height = metadata.get("height")
-    os.system("cp *mp4 thumb.mp4")
-    os.system("ffmpeg -i thumb.mp4 -vframes 1 -an -s 480x360 -ss 5 thumb.jpg")
-    thumb_image = "thumb.jpg"
-    c_time = time.time()
-    await event.client.send_file(
-        event.chat_id,
-        loa,
-        force_document=False,
-        thumb=thumb_image,
-        allow_cache=False,
-        caption=query,
-        supports_streaming=True,
-        reply_to=reply_to_id,
-        attributes=[
-            DocumentAttributeVideo(
-                duration=duration,
-                w=width,
-                h=height,
-                round_message=False,
-                supports_streaming=True,
-            )
-        ],
-        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-            progress(d, t, event, c_time, "[UPLOAD]", loa)
-        ),
-    )
-    await event.edit(f"**{query}** `Uploaded Successfully..!`")
-    os.remove(thumb_image)
-    os.system("rm -rf *.mkv")
-    os.system("rm -rf *.mp4")
-    os.system("rm -rf *.webm")
+    try:
+        loa = l[0]
+        metadata = extractMetadata(createParser(loa))
+        duration = 0
+        width = 0
+        height = 0
+        if metadata.has("duration"):
+            duration = metadata.get("duration").seconds
+        if metadata.has("width"):
+            width = metadata.get("width")
+        if metadata.has("height"):
+            height = metadata.get("height")
+        os.system("cp *mp4 thumb.mp4")
+        os.system("ffmpeg -i thumb.mp4 -vframes 1 -an -s 480x360 -ss 5 thumb.jpg")
+        thumb_image = "thumb.jpg"
+        c_time = time.time()
+        await event.client.send_file(
+            event.chat_id,
+            loa,
+            force_document=False,
+            thumb=thumb_image,
+            allow_cache=False,
+            caption=query,
+            supports_streaming=True,
+            reply_to=reply_to_id,
+            attributes=[
+                DocumentAttributeVideo(
+                    duration=duration,
+                    w=width,
+                    h=height,
+                    round_message=False,
+                    supports_streaming=True,
+                )
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(d, t, event, c_time, "[UPLOAD]", loa)
+            ),
+        )
+        await event.edit(f"**{query}** `Uploaded Successfully..!`")
+        os.remove(thumb_image)
+        os.system("rm -rf *.mkv")
+        os.system("rm -rf *.mp4")
+        os.system("rm -rf *.webm")
+    except BaseException:
+        os.remove(thumb_image)
+        os.system("rm -rf *.mkv")
+        os.system("rm -rf *.mp4")
+        os.system("rm -rf *.webm")
+        return
 
 
 @register(outgoing=True, pattern=r"^\.smd(?: |$)(.*)")
