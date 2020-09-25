c5a15815729a3ac3b45f716c293fdd9e63f36020
Alfiananda <65683445+alfiananda88@users.noreply.github.com>
2020-09-26 01:56:30 +0700

    Update ssvideo.py

--- userbot/modules/ssvideo.py
+++ userbot/modules/ssvideo.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2020 Alfiananda P.A
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
 import os
 
 from userbot import CMD_HELP, bot
